0ec46104ee3631f2d68ef8750c16453cb65a2a9a
JordiRuspira <34830607+JordiRuspira@users.noreply.github.com>
2023-02-11 01:27:15 +0100

    Update cosmos_sankey_new.py

--- cosmos_sankey_new.py
+++ cosmos_sankey_new.py
@@ -15,8 +15,7 @@ import random
 import plotly.io as pio
 import streamlit_autorefresh 
 
-try:
-	st.set_page_config(
+st.set_page_config(
 		page_title="Cosmos Validators - Sankey chart",
 		page_icon=":atom_symbol:",
 		layout="wide",
@@ -24,17 +23,17 @@ try:
 	)
 
 
-	st.title(":atom_symbol: Cosmos Sankey :atom_symbol:")
+st.title(":atom_symbol: Cosmos Sankey :atom_symbol:")
 
 
-	st.success("This app only contains a chart! Please select a validator ")
-	st.text("")
-	st.subheader('Streamlit App by [Jordi R.](https://twitter.com/RuspiTorpi/). Powered by Flipsidecrypto')
-	st.text("")
-	st.markdown('Hi there. This streamlit app displays a Sankey chart showing redelegations from a selected validator to the rest of validators, starting on 2023-02-05.' )   
+st.success("This app only contains a chart! Please select a validator ")
+st.text("")
+st.subheader('Streamlit App by [Jordi R.](https://twitter.com/RuspiTorpi/). Powered by Flipsidecrypto')
+st.text("")
+st.markdown('Hi there. This streamlit app displays a Sankey chart showing redelegations from a selected validator to the rest of validators, starting on 2023-02-05.' )   
 
 
-	st.markdown(
+st.markdown(
 			f"""
 	<style>
 		.reportview-container .main .block-container{{
@@ -57,16 +56,16 @@ try:
 
 
 
-	API_KEY = st.secrets["API_KEY"]
+API_KEY = st.secrets["API_KEY"]
 
 
 
 	  
-	SQL_QUERY = """   select distinct address, label, RANK() OVER (ORDER BY delegator_shares DESC) AS RANK from cosmos.core.fact_validators 
+SQL_QUERY = """   select distinct address, label, RANK() OVER (ORDER BY delegator_shares DESC) AS RANK from cosmos.core.fact_validators 
 	 	order by rank
-	"""  
+"""  
 
-	SQL_QUERY_2 = """ 
+SQL_QUERY_2 = """ 
 with table_8 as (
 select distinct tx_id from cosmos.core.fact_msg_attributes
 where tx_succeeded = 'TRUE'
@@ -120,151 +119,142 @@ left join table_12 d
 on a.tx_id = d.tx_id 
 group by from_validator, to_validator, from_validator_rank, to_validator_rank
 	  
-	"""  
+"""  
 	 
 
-	TTL_MINUTES = 15
-	# return up to 100,000 results per GET request on the query id
-	PAGE_SIZE = 100000
-	# return results of page 1
-	PAGE_NUMBER = 1
+TTL_MINUTES = 15
+# return up to 100,000 results per GET request on the query id
+PAGE_SIZE = 100000
+# return results of page 1
+PAGE_NUMBER = 1
 
-	def create_query():
-		r = requests.post(
+def create_query():
+	r = requests.post(
 			'https://node-api.flipsidecrypto.com/queries', 
 			data=json.dumps({
 				"sql": SQL_QUERY,
 				"ttlMinutes": TTL_MINUTES
 			}),
 			headers={"Accept": "application/json", "Content-Type": "application/json", "x-api-key": API_KEY},
-		)
-		if r.status_code != 200:
-			raise Exception("Error creating query, got response: " + r.text + "with status code: " + str(r.status_code))
+	)
+	if r.status_code != 200:
+		raise Exception("Error creating query, got response: " + r.text + "with status code: " + str(r.status_code))
 		
-		return json.loads(r.text)    
+	return json.loads(r.text)    
 	 
-	def create_query_2():
-		r = requests.post(
+def create_query_2():
+	r = requests.post(
 			'https://node-api.flipsidecrypto.com/queries', 
 			data=json.dumps({
 				"sql": SQL_QUERY_2,
 				"ttlMinutes": TTL_MINUTES
 			}),
 			headers={"Accept": "application/json", "Content-Type": "application/json", "x-api-key": API_KEY},
-		)
-		if r.status_code != 200:
-			raise Exception("Error creating query, got response: " + r.text + "with status code: " + str(r.status_code))
+	)
+	if r.status_code != 200:
+		raise Exception("Error creating query, got response: " + r.text + "with status code: " + str(r.status_code))
 		
-		return json.loads(r.text)    
+	return json.loads(r.text)    
 	 
 
-	def get_query_results(token):
-		r = requests.get(
+def get_query_results(token):
+	r = requests.get(
 			'https://node-api.flipsidecrypto.com/queries/{token}?pageNumber={page_number}&pageSize={page_size}'.format(
 			  token=token,
 			  page_number=PAGE_NUMBER,
 			  page_size=PAGE_SIZE
 			),
 			headers={"Accept": "application/json", "Content-Type": "application/json", "x-api-key": API_KEY}
-		)
-		if r.status_code != 200:
-			raise Exception("Error getting query results, got response: " + r.text + "with status code: " + str(r.status_code))
+	)
+	if r.status_code != 200:
+		raise Exception("Error getting query results, got response: " + r.text + "with status code: " + str(r.status_code))
 		
-		data = json.loads(r.text)
-		if data['status'] == 'running':
-			time.sleep(10)
-			return get_query_results(token)
+	data = json.loads(r.text)
+	if data['status'] == 'running':
+		time.sleep(10)
+		return get_query_results(token)
 
-		return data
+	return data
 
 
 
-	def get_query_results_2(token):
-		r = requests.get(
+def get_query_results_2(token):
+	r = requests.get(
 			'https://node-api.flipsidecrypto.com/queries/{token}?pageNumber={page_number}&pageSize={page_size}'.format(
 			  token=token,
 			  page_number=PAGE_NUMBER,
 			  page_size=PAGE_SIZE
-			),
+		),
 			headers={"Accept": "application/json", "Content-Type": "application/json", "x-api-key": API_KEY}
-		)
-		if r.status_code != 200:
-			raise Exception("Error getting query results, got response: " + r.text + "with status code: " + str(r.status_code))
+	)
+	if r.status_code != 200:
+		raise Exception("Error getting query results, got response: " + r.text + "with status code: " + str(r.status_code))
 		
-		data = json.loads(r.text)
-		if data['status'] == 'running':
-			time.sleep(10)
-			return get_query_results_2(token)
+	data = json.loads(r.text)
+	if data['status'] == 'running':
+		time.sleep(10)
+		return get_query_results_2(token)
 
-		return data
+	return data
 
 
-	query_2 = create_query_2()
-	token_2 = query_2.get('token')
-	data2 = get_query_results_2(token_2) 
-	df2 = pd.DataFrame(data2['results'], columns = ['FROM_VALIDATOR', 'TO_VALIDATOR','FROM_VALIDATOR_RANK','TO_VALIDATOR_RANK','AMOUNT_REDELEGATED'])
+query_2 = create_query_2()
+token_2 = query_2.get('token')
+data2 = get_query_results_2(token_2) 
+df2 = pd.DataFrame(data2['results'], columns = ['FROM_VALIDATOR', 'TO_VALIDATOR','FROM_VALIDATOR_RANK','TO_VALIDATOR_RANK','AMOUNT_REDELEGATED'])
 
 
 	  
 
 
 
-	query = create_query()
-	token = query.get('token')
-	data1 = get_query_results(token)
-	df1 = pd.DataFrame(data1['results'], columns = ['ADDRESS', 'LABEL','RANK']) 
+query = create_query()
+token = query.get('token')
+data1 = get_query_results(token)
+df1 = pd.DataFrame(data1['results'], columns = ['ADDRESS', 'LABEL','RANK']) 
 
-	randcolor = []
-	for i in range(1,len(df1['LABEL']) + 1):
+randcolor = []
+for i in range(1,len(df1['LABEL']) + 1):
 	 
-		randcolor.append("#{:06x}".format(random.randint(0, 0xFFFFFF))) 
+	randcolor.append("#{:06x}".format(random.randint(0, 0xFFFFFF))) 
 		
-	df1['COLOR'] = randcolor
+df1['COLOR'] = randcolor
 
 
-	keys_list =  df1['RANK']
-	values_list = df1['LABEL']
-	zip_iterator = zip(keys_list, values_list) 
-	a_dictionary = dict(zip_iterator)
+keys_list =  df1['RANK']
+values_list = df1['LABEL']
+zip_iterator = zip(keys_list, values_list) 
+a_dictionary = dict(zip_iterator)
+
+df3 = pd.DataFrame(a_dictionary.items(), columns = ['RANK','LABEL'], index = keys_list)
+df3.index = df3.index
+df3 = df3.sort_index()
 
 
-
-	df3 = pd.DataFrame(a_dictionary.items(), columns = ['RANK','LABEL'], index = keys_list)
-	df3.index = df3.index
-	df3 = df3.sort_index()
-
-
-
-
-
-
-	with st.container():
+with st.container():
 
 		
-		validator_choice = st.selectbox("Choose a validator", options = df2['FROM_VALIDATOR'].unique() )
+	validator_choice = st.selectbox("Choose a validator", options = df2['FROM_VALIDATOR'].unique() )
 
 		
-		df_filtered = df2[df2['FROM_VALIDATOR'] == validator_choice]
-		df_filtered['Link color'] = 'rgba(127, 194, 65, 0.2)'
-		df_filtered['FROM_VALIDATOR_RANK'] = df_filtered['FROM_VALIDATOR_RANK']-1
-		df_filtered['TO_VALIDATOR_RANK'] = df_filtered['TO_VALIDATOR_RANK'] - 1
+	df_filtered = df2[df2['FROM_VALIDATOR'] == validator_choice]
+	df_filtered['Link color'] = 'rgba(127, 194, 65, 0.2)'
+	df_filtered['FROM_VALIDATOR_RANK'] = df_filtered['FROM_VALIDATOR_RANK']-1
+	df_filtered['TO_VALIDATOR_RANK'] = df_filtered['TO_VALIDATOR_RANK'] - 1
 
-		link = dict(source = df_filtered['FROM_VALIDATOR_RANK'].values , target = df_filtered['TO_VALIDATOR_RANK'].values, value = df_filtered['AMOUNT_REDELEGATED'], color = df1['COLOR'])
-		node = dict(label = df3['LABEL'].values, pad = 35, thickness = 10)
+	link = dict(source = df_filtered['FROM_VALIDATOR_RANK'].values , target = df_filtered['TO_VALIDATOR_RANK'].values, value = df_filtered['AMOUNT_REDELEGATED'], color = df1['COLOR'])
+	node = dict(label = df3['LABEL'].values, pad = 35, thickness = 10)
 
 	 
 		
 		 
-		data = go.Sankey(link = link, node = node)
-		fig = go.Figure(data)
-		fig.update_layout(
+	data = go.Sankey(link = link, node = node)
+	fig = go.Figure(data)
+	fig.update_layout(
 			hovermode = 'x', 
 			font = dict(size = 20, color = 'white'), 
 			paper_bgcolor= 'rgba(0,0,0,0)',
 			width=1000, height=1300
-		) 
+	) 
 		
-		st.plotly_chart(fig, use_container_width=True) 
-
-except: 
-   st_autorefresh(interval=10)
+	st.plotly_chart(fig, use_container_width=True) 
